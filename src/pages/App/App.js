cf3a187c40b7f190a1a71d3d8ebb4cd02563464a
Anthony Ashcraft <109315220+anthonyash91@users.noreply.github.com>
2023-05-30 22:01:02 -0400

    first commit

---
+++ src/pages/App/App.js
@@ -0,0 +1,13 @@
+import { Route, Routes } from 'react-router-dom';
+
+function App() {
+  return (
+    <>kjhkjh
+      {/* <Routes>
+          <Route path="/" element={<Main />} />
+        </Routes> */}
+    </>
+  );
+}
+
+export default App;
