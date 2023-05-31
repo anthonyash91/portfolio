6679741c205dddf4d55c5ef41498e5794e1032bd
Anthony Ashcraft <109315220+anthonyash91@users.noreply.github.com>
2023-05-31 16:25:41 -0400

    first section done

--- src/pages/App/App.js
+++ src/pages/App/App.js
@@ -1,11 +1,24 @@
-import { Route, Routes } from 'react-router-dom';
-
 function App() {
   return (
-    <>kjhkjh
-      {/* <Routes>
-          <Route path="/" element={<Main />} />
-        </Routes> */}
+    <>
+      <div id="container">
+        <div className="section">
+          <div className="sub-section">
+            <div className="name">
+              <h1>Anthony Ashcraft 👋</h1>
+              <span />Junior Full-Stack Engineer
+            </div>
+
+            <div className="description">I'm a passionate full-stack Software Engineer (with a focus on frontend) with experience in creating beautiful, user-friendly, UX-rich web applications.</div>
+          </div>
+
+          <div className="sub-section photo">
+            <div className="icon">
+              <div className="icon-image" />
+            </div>
+          </div>
+        </div>
+      </div>
     </>
   );
 }
